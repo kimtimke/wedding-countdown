4a0e18985cff1ba298a86dd56913e766635efe59
Namgirl <seebachkim@gmail.com>
2023-04-10 17:59:14 +0200

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Timer from "./Timer";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
         <Timer />
       </header>
     </div>
---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="Header">
+      <h1> Nikita und Eric</h1>
+    </div>
+  );
+}
